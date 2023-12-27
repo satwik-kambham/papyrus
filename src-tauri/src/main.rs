84f8ab809eaec88b975790af102743d9422079cc
Satwik Kambham <satwik.kambham@gmail.com>
2023-12-27 08:24:19 +0530

    Add file handling functionality and open file dialog

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+pub mod core;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            core::file_handling::read_file_content
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,8 +1,26 @@
 <script setup lang="ts">
+import { open } from "@tauri-apps/api/dialog";
+import { invoke } from "@tauri-apps/api";
+
+async function open_file() {
+  const selected = await open();
+  if (selected !== null) {
+    // user selected a single file
+    console.log(selected);
+    invoke("read_file_content", { path: selected })
+      .then((content) => {
+        console.log(content);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+}
 </script>
 
 <template>
-    <div class="bg-slate-900 p-1">
-        MenuBar
-    </div>
+  <div class="bg-slate-900 p-1">
+    MenuBar
+    <button type="button" @click="open_file()">Open File</button>
+  </div>
 </template>
