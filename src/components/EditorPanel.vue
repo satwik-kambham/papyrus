c7082fac0a92626329eb543fb7d5ad236dd885b2
Satwik Kambham <satwik.kambham@gmail.com>
2024-01-29 12:27:41 +0530

    Improved tab UI

--- src/components/EditorPanel.vue
+++ src/components/EditorPanel.vue
@@ -33,6 +33,7 @@ const selectionHighlights = ref<
 let selecting = false;
 
 const gutterWidth = ref(0);
+const hoverTabIndex = ref(-1);
 
 onUpdated(() => {
   gutterWidth.value = gutterElement.value?.getBoundingClientRect().width ?? 0;
@@ -68,6 +69,7 @@ function resetState() {
   selecting = false;
 
   gutterWidth.value = 0;
+  hoverTabIndex.value = -1;
 }
 
 async function switchBuffer(index: number) {
@@ -791,25 +793,24 @@ async function key_event(e: KeyboardEvent) {
       class="flex overflow-x-auto custom-scrollbar z-30 bg-atom-bg-dark border-b-[1px] border-atom-black"
     >
       <div
-        class="px-2 py-2 border-r-[1px] min-w-52 justify-center flex-shrink-0 border-r-atom-black whitespace-nowrap cursor-pointer select-none flex"
+        class="px-2 py-1 border-r-[1px] min-w-64 justify-center flex-shrink-0 border-r-atom-black whitespace-nowrap cursor-pointer select-none flex"
         v-for="(editor, index) in workspaceStore.openEditors"
         :key="index"
+        @mouseover="hoverTabIndex = index"
+        @mouseleave="hoverTabIndex = -1"
         @click="() => workspaceStore.switchEditor(index)"
         :class="{
-          'bg-atom-bg text-atom-text border-l-2 border-atom-highlight':
+          'bg-atom-bg text-atom-text border-l-2 border-atom-primary':
             index == workspaceStore.currentEditorIndex,
           'text-atom-text-light': index != workspaceStore.currentEditorIndex,
         }"
       >
+        <div class="grow"></div>
         {{ editor.entry?.name }}
+        <div class="grow"></div>
+
         <div
-          class="px-1 text-atom-primary"
-          v-if="workspaceStore.openEditors[index].unsavedChanges"
-        >
-          U
-        </div>
-        <div
-          class="px-1 text-atom-text-light hover:text-atom-text mx-1"
+          class="px-1 text-atom-text-light hover:text-black hover:bg-atom-primary mx-1 rounded-sm"
           @click="
             async (e) => {
               e.stopPropagation();
@@ -817,7 +818,25 @@ async function key_event(e: KeyboardEvent) {
             }
           "
         >
-          x
+          <div
+            class="text-atom-primary w-2"
+            v-if="
+              workspaceStore.openEditors[index].unsavedChanges &&
+              hoverTabIndex != index
+            "
+          >
+            U
+          </div>
+          <div
+            v-else
+            class="transition transition-tranform transition-opacity duration-300 mb-1.5 w-2 text-center"
+            :class="{
+              'opacity-100 scale-100': hoverTabIndex == index,
+              'opacity-0 scale-0': hoverTabIndex != index,
+            }"
+          >
+            x
+          </div>
         </div>
       </div>
     </div>
